9770c8cfa79093e525a40e153dd1665794d1745c
宝码 <nongyoubao@alibaba-inc.com>
2015-05-22 17:51:52 +0800

    加大偏移量

--- pages/mouse.js
+++ pages/mouse.js
@@ -1,12 +1,12 @@
 module.exports = {
   enter: function(elem) {
-    var x = y = 1;
+    var x = y = 5;
     return this.client
       .moveToElement(elem, x, y)
       .pause(600)
   },
   leave: function(elem) {
-    var x = y = 1;
+    var x = y = -10;
     return this.client
       .moveToElement(elem, x, y)
       .pause(600)
